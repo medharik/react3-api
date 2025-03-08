6c8f9de3b3bfbbbdb29c58266040213ff2fff13d
medharik <med.harik@gmail.com>
2025-03-08 23:37:20 +0000

    merge 2

--- src/apis/produitApi.js
+++ src/apis/produitApi.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 // const URL = "https://6679da7d18a459f63951a26a.mockapi.io/produits";
-<<<<<<< HEAD
+// <<<<<<< HEAD
 export const URL = "http://127.0.0.1:8000/api/produits";
 export const URL_BASE_IMAGE = "http://127.0.0.1:8000/storage";
 //WEB SERVICE  PROVIDER  (API)
-=======
-const URL = "http://127.0.0.1:8000/api/produits";
+// =======
+// const URL = "http://127.0.0.1:8000/api/produits";
 
 // //WEB SERVICE  PROVIDER  (API)
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/produits';
 
 
->>>>>>> reducer-test
+// >>>>>>> reducer-test
 
 export const all = async () => {
   try {
     const resp = await axios.get(URL);
-    console.log(resp.data);
+    // console.log(resp.data);
     return resp.data;
   } catch (error) {
     console.error("erreur all :", error);
