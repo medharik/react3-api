6d10b3c076bae8648e99c2369d9ecbc49488aae5
medharik <med.harik@gmail.com>
2025-03-17 03:07:14 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useReducer, useRef, useState } from "react";
 import {
   all,
@@ -9,6 +10,8 @@ import {
 } from "./apis/produitApi";
 import Form from "./components/Form";
 import Liste from "./components/Liste";
+import { useTheme } from "./components/ThemeProvider";
+
 
 function App() {
   const [produits, setProduits] = useState([]);
@@ -124,9 +127,16 @@ case "EDIT":
       )
     );
   }, [mc, produits]);
-
+const {theme,setTheme}=useTheme();
   return (
     <>
+   <nav>
+   <div className=" d-flex justify-content-end p-1"><button onClick={()=>setTheme(theme==='primary'? 'dark':'primary')}>
+      
+      { theme==='primary'?  <i class="bi bi-lightbulb-fill"></i>:<i class="bi bi-lightbulb"></i>
+}
+    </button></div>
+   </nav>
       <div className="container text-center bg-light">
       {/* <div className={`alert alert-${notice.color}`}>{notice.texte}</div> */}
         {produit.id && (
